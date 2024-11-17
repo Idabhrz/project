6a5f762bb3eb70de56a962cac2cf596bea297a0e
Idabhrz <Idabhrz@gmail.com>
2024-11-17 18:23:32 +0330

    Update prime.mirror.dt.cs

--- prime.mirror.dt.cs
+++ prime.mirror.dt.cs
@@ -1,182 +1,208 @@
 using System;
 
-class Program
+namespace SimpleFactoryExample
 {
-    static void Main(string[] args)
+    public interface INumberOperation
     {
-        while (true)
+        void Execute();
+    }
+
+    public class PrimeNumberOperation : INumberOperation
+    {
+        public void Execute()
         {
-            Console.WriteLine("Choose an option:");
-            Console.WriteLine("1 - Check Prime Number");
-            Console.WriteLine("2 - Check Mirror Number");
-            Console.WriteLine("3 - Decision Tree");
-            Console.WriteLine("0 - Exit");
-            string choice = Console.ReadLine();
-
-            var factory = new FunctionalityFactory();
-
-            switch (choice)
+            Console.Write("Enter a number: ");
+            if (int.TryParse(Console.ReadLine(), out int number))
             {
-                case "1":
-                    factory.CreateFunctionality("PrimeNumber").Execute();
-                    break;
-                case "2":
-                    factory.CreateFunctionality("MirrorNumber").Execute();
-                    break;
-                case "3":
-                    factory.CreateFunctionality("DecisionTree").Execute();
-                    break;
-                case "0":
-                    return;
-                default:
-                    Console.WriteLine("Invalid choice, please try again.");
-                    break;
-            }
-        }
-    }
-}
-
-public interface IFunctionality
-{
-    void Execute();
-}
-
-public class PrimeNumberFunctionality : IFunctionality
-{
-    public void Execute()
-    {
-        Console.Write("Enter a number: ");
-        if (int.TryParse(Console.ReadLine(), out int number))
-        {
-            bool isPrime = IsPrime(number);
-            Console.WriteLine(isPrime ? $"{number} is a prime number." : $"{number} is not a prime number.");
-        }
-        else
-        {
-            Console.WriteLine("Invalid input. Please enter a valid number.");
-        }
-    }
-
-    private bool IsPrime(int number)
-    {
-        if (number <= 1) return false;
-        for (int i = 2; i <= Math.Sqrt(number); i++)
-        {
-            if (number % i == 0) return false;
-        }
-        return true;
-    }
-}
-
-public class MirrorNumberFunctionality : IFunctionality
-{
-    public void Execute()
-    {
-        Console.Write("Enter a number: ");
-        if (int.TryParse(Console.ReadLine(), out int number))
-        {
-            bool isMirror = IsMirrorNumber(number);
-            Console.WriteLine(isMirror ? $"{number} is a mirror number." : $"{number} is not a mirror number.");
-        }
-        else
-        {
-            Console.WriteLine("Invalid input. Please enter a valid number.");
-        }
-    }
-
-    private bool IsMirrorNumber(int number)
-    {
-        int original = number;
-        int reversed = 0;
-
-        while (number > 0)
-        {
-            int digit = number % 10;
-            reversed = reversed * 10 + digit;
-            number /= 10;
-        }
-
-        return original == reversed;
-    }
-}
-
-public class DecisionTreeFunctionality : IFunctionality
-{
-    public void Execute()
-    {
-        Console.WriteLine("Answer the following questions with 'yes' or 'no':");
-
-        Console.Write("Is it raining? ");
-        string answer1 = Console.ReadLine().ToLower().Trim();
-
-        Console.Write("Do you have an umbrella? ");
-        string answer2 = Console.ReadLine().ToLower().Trim();
-
-        Console.Write("Are you going outside? ");
-        string answer3 = Console.ReadLine().ToLower().Trim();
-
-        if (answer1 == "yes")
-        {
-            if (answer2 == "yes")
-            {
-                Console.WriteLine("You can go outside with your umbrella.");
-            }
-            else if (answer2 == "no")
-            {
-                if (answer3 == "yes")
+                if (number < 1000)
                 {
-                    Console.WriteLine("You should stay inside.");
-                }
-                else if (answer3 == "no")
-                {
-                    Console.WriteLine("You are safe at home.");
+                    Console.WriteLine("Checking for primes below 1000...");
+                    CheckPrimesBelow1000();
                 }
                 else
                 {
-                    Console.WriteLine("Invalid input for going outside.");
+                    Console.WriteLine("Checking for primes 1000 and above...");
+                    CheckPrimesAbove1000(number);
                 }
             }
             else
             {
-                Console.WriteLine("Invalid input for umbrella.");
+                Console.WriteLine("Invalid input. Please enter a valid number.");
             }
         }
-        else if (answer1 == "no")
+
+        private void CheckPrimesBelow1000()
         {
-            if (answer3 == "yes")
+            for (int i = 2; i < 1000; i++)
             {
-                Console.WriteLine("You can go outside.");
+                if (IsPrime(i))
+                {
+                    Console.WriteLine(i);
+                }
             }
-            else if (answer3 == "no")
+        }
+
+        private void CheckPrimesAbove1000(int number)
+        {
+            if (IsPrime(number))
             {
-                Console.WriteLine("You are safe at home.");
+                Console.WriteLine($"{number} is a prime number.");
             }
             else
             {
-                Console.WriteLine("Invalid input for going outside.");
+                Console.WriteLine($"{number} is not a prime number.");
             }
         }
-        else
+
+        private bool IsPrime(int num)
         {
-            Console.WriteLine("Invalid input for rain question.");
+            if (num <= 1) return false;
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0) return false;
+            }
+            return true;
         }
     }
-}
 
-public class FunctionalityFactory
-{
-    public IFunctionality CreateFunctionality(string type)
+    public class PalindromeNumberOperation : INumberOperation
     {
-        switch (type)
+        public void Execute()
         {
-            case "PrimeNumber":
-                return new PrimeNumberFunctionality();
-            case "MirrorNumber":
-                return new MirrorNumberFunctionality();
-            case "DecisionTree":
-                return new DecisionTreeFunctionality();
-            default:
-                throw new ArgumentException("Invalid functionality type");
+            Console.Write("Enter a number: ");
+            if (int.TryParse(Console.ReadLine(), out int number))
+            {
+                if (number < 1000)
+                {
+                    Console.WriteLine("Checking for palindromes below 1000...");
+                    CheckPalindromesBelow1000();
+                }
+                else
+                {
+                    Console.WriteLine("Checking for palindromes 1000 and above...");
+                    CheckPalindrome(number);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+        }
+
+        private void CheckPalindromesBelow1000()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                if (IsPalindrome(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        private void CheckPalindrome(int number)
+        {
+            if (IsPalindrome(number))
+            {
+                Console.WriteLine($"{number} is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is not a palindrome.");
+            }
+        }
+
+        private bool IsPalindrome(int num)
+        {
+            string str = num.ToString();
+            char[] arr = str.ToCharArray();
+            Array.Reverse(arr);
+            return str == new string(arr);
+        }
+    }
+
+    public class DecisionTreeOperation : INumberOperation
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Answer the following questions with 'yes' or 'no':");
+
+            if (AskQuestion("Is it an animal?"))
+            {
+                if (AskQuestion("Is it a mammal?"))
+                {
+                    if (AskQuestion("Does it have stripes?"))
+                    {
+                        Console.WriteLine("It's a tiger!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("It's a dog!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("It's a reptile!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("It's a rock!");
+            }
+        }
+
+        private bool AskQuestion(string question)
+        {
+            Console.Write($"{question} ");
+            string answer = Console.ReadLine()?.ToLower();
+            return answer == "yes";
+        }
+    }
+
+    public static class NumberOperationFactory
+    {
+        public static INumberOperation CreateOperation(int choice)
+        {
+            switch (choice)
+            {
+                case 1:
+                    return new PrimeNumberOperation();
+                case 2:
+                    return new PalindromeNumberOperation();
+                case 3:
+                    return new DecisionTreeOperation();
+                default:
+                    throw new ArgumentException("Invalid choice.");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose an operation:");
+            Console.WriteLine("1. Prime Number Operation");
+            Console.WriteLine("2. Palindrome Number Operation");
+            Console.WriteLine("3. Decision Tree Operation");
+            Console.Write("Enter your choice: ");
+
+            if (int.TryParse(Console.ReadLine(), out int choice))
+            {
+                try
+                {
+                    INumberOperation operation = NumberOperationFactory.CreateOperation(choice);
+                    operation.Execute();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
         }
     }
 }
